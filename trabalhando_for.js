b3891e3ea6adc7d1b6ac15e2531b4d569c70af63
igordsa <igrfer@outlook.com>
2020-10-18 18:52:13 -0300

    Exemplo de for finalizado

---
+++ trabalhando_for.js
@@ -0,0 +1,31 @@
+var convidados = [
+    {
+        nome: "Pedro",
+        idade: 20,
+        bebeRefrigerante: false,
+        bebeCerveja: true
+    },
+    {
+        nome: "Igor",
+        idade: 19,
+        bebeRefrigerante: true,
+        bebeCerveja: false,
+    },
+    {
+        nome: "Rodrigo",
+        idade: 19,
+        bebeRefrigerante: true,
+        bebeCerveja: true
+    }
+]
+
+var comprarCerveja = false
+
+console.log(convidados.length)
+
+for (var i = 0; i < convidados.length; i++) {
+    console.log(convidados[i].nome, convidados[i].bebeCerveja)
+    if (convidados[i].bebeCerveja) {
+        comprarCerveja = true
+    }
+}
